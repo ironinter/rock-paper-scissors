1af10e28c76f339b636a246f9d4421e1db2b0772
ironinter <anonymousk539@gmail.com>
2026-02-19 18:10:01 +0530

    Add logic for all functions of  rock-paper-scissors

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
-console.log("something");
+function getComputerChoice() {
+    let choice = Math.random();
+
+    if (choice < 0.33) {
+        return "Rock";
+    } else if (choice < 0.66) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+function getHumanChoice() {
+    let choice = prompt("Enter your choice: ");
+    return choice;
+}
+
+
+function playRound() {
+    let humanChoice = getHumanChoice().toLowerCase();
+    let computerChoice = getComputerChoice();
+
+    if (humanChoice === 'scissors' && computerChoice === 'Paper' || humanChoice === 'paper' && computerChoice === 'Rock' || humanChoice === 'rock' && computerChoice === 'Scissors') {
+        humanScore++;
+        result = `${humanChoice} beats ${computerChoice}. You Win!`;
+        return result;     
+    } else if (humanChoice === computerChoice.toLowerCase()) {
+        return "Its a draw."
+    } 
+
+    computerScore++;
+    result = `${computerChoice} beats ${humanChoice}. You Lose!`;
+    return result;
+}
+
+// Declare Scores
+let humanScore = 0, computerScore = 0;
+
+function playGame() {
+    // hChoice = getHumanChoice();
+    // cChoice = getComputerChoice();
+    
+    // Play round 5 times
+    for (let i = 0; i < 5; i++) {
+        result = playRound();
+        console.log(result);
+    }
+
+    console.log(`Your Score: ${humanScore} v/s Computer Score: ${computerScore}`);
+
+    if (humanScore > computerScore) {
+        console.log("YOU WIN!!!");
+    } else if (humanScore === computerScore) {
+        console.log("ITS A DRAW.");
+    } else {
+        console.log("YOU LOSE");
+    }
+
+}
+
+playGame();
